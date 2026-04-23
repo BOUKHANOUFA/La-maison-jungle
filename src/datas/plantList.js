ba7b42265daa7f4469b64d8d18fb469f63700d68
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-04-23 17:08:30 +0200

    shoppigList-ajout+rater

---
+++ src/datas/plantList.js
@@ -0,0 +1,32 @@
+export const plantList = [
+    {
+    name: 'monstera',
+    category: 'classique',
+    id: '1ed',
+    isBestSale: true
+  },
+  {
+    name: 'ficus lyrata',
+    category: 'classique',
+    id: '2ab',
+    isBestSale: false
+  },
+  {
+    name: 'pothos argenté',
+    category: 'classique',
+    id: '3sd',
+    isBestSale: false
+  },
+  {
+    name: 'yucca',
+    category: 'exterieur',
+    id: '4kk',
+    isBestSale: true
+  },
+  {
+    name: 'palmier',
+    category: 'exterieur',
+    id: '5pl',
+    isBestSale: false
+  }
+]
